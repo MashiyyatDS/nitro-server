8455c359332ebf0c609c98a182c5ab1f86e8ff27
MashiyyatDS <delossantos.mashiyyat@gmail.com>
2024-08-22 18:06:36 +0800

    chore: minor refactoring

--- server/api/user/insert.ts
+++ server/api/user/insert.ts
@@ -7,9 +7,13 @@ export default defineEventHandler(async (event) => {
 
 	const { first_name, middle_name, last_name, email, password } = await readBody(event)
 
-	await db.sql`INSERT INTO users (id, first_name, middle_name, last_name, email, password) VALUES(${userId}, ${first_name}, ${middle_name}, ${last_name}, ${email}, ${password})`
+	try {
+		await db.sql`INSERT INTO users (id, first_name, middle_name, last_name, email, password) VALUES(${userId}, ${first_name}, ${middle_name}, ${last_name}, ${email}, ${password})`
 
-	const { rows: user } = await db.sql`SELECT * FROM users WHERE id = ${userId}`
+		const { rows: user } = await db.sql`SELECT * FROM users WHERE id = ${userId}`
 
-	return user[0]
+		return user[0]
+	} catch (error) {
+		return 'Something went wrong...'
+	}
 })
