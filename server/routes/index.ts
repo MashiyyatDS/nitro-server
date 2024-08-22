abd4612b0204753a3456bb8df1174038683b3127
MashiyyatDS <delossantos.mashiyyat@gmail.com>
2024-08-22 18:25:43 +0800

    chore: test deployment

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,13 +1,16 @@
 export default eventHandler(async (event) => {
-	const db = useDatabase()
-	await db.sql`CREATE TABLE IF NOT EXISTS users (
-        id TEXT PRIMARY KEY,
-        first_name TEXT,
-        middle_name TEXT,
-        last_name TEXT,
-        email TEXT,
-        password TEXT
-    )`
+	//const db = useDatabase()
+	//await db.sql`CREATE TABLE IF NOT EXISTS users (
+	//    id TEXT PRIMARY KEY,
+	//    first_name TEXT,
+	//    middle_name TEXT,
+	//    last_name TEXT,
+	//    email TEXT,
+	//    password TEXT
+	//)`
 
-	return 'Nitro Server Success'
+	//return 'Nitro Server Success'
+	return {
+		message: 'Success',
+	}
 })
